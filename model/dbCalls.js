56d45b869b4402284f9f94168ed5d19400b887ff
Ken Ingram <ken.ingram@gmail.com>
2021-02-20 10:12:58 -0800

    Remove commented code

--- model/dbCalls.js
+++ model/dbCalls.js
@@ -1,19 +1,6 @@
-// const sqlite3 = require('sqlite3').verbose()
-// const db = new sqlite3.Database('sunshine.db')
 const sqlite3 = require('sqlite3')
 const { open } = require('sqlite')
 sqlite3.verbose()
-
-
-/**
- * We have already determined the database file, so open it.
- */
-// (async () => {
-//   const db = await open({
-//     filename: './sunshine.db',
-//     driver: sqlite3.Database
-//   })
-// })()
 
 async function openDB(filename) {
   return open({
@@ -28,36 +15,10 @@ async function SELECT(sql) {
   return result
 }
 
-
-
-// async function SELECT(sql, callback) {
-//   let results
-//   db.serialize(() => {
-//     db.each(sql, async (err, row) => {
-//       results = await row
-//       callback(results)
-//     })
-//   })
-//   db.close()
-// }
-
-// function showData(data) {
-//   let cols = Object.keys(data)
-//   // console.log(`${cols[0]}: ${data[cols[0]]}, ${cols[1]}: ${data[cols[1]]}`)
-//   console.log(data)
-//   return data
-// }
-
-// async function test() {
-//   let output = await SELECT("Select * from test")
-//   console.log(output)
-// }
-
 async function test() {
   let output = await SELECT("Select * from test")
   console.log(output)
 }
-
 
 test()
 
