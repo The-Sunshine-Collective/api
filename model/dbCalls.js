a1b035291ebfa00293e11c771bd73d56dca8547c
Ken Ingram <ken.ingram@gmail.com>
2021-02-20 10:10:49 -0800

    Adjust dbCalls.js to use Promisified wrapper

--- model/dbCalls.js
+++ model/dbCalls.js
@@ -5,6 +5,16 @@ const { open } = require('sqlite')
 sqlite3.verbose()
 
 
+/**
+ * We have already determined the database file, so open it.
+ */
+// (async () => {
+//   const db = await open({
+//     filename: './sunshine.db',
+//     driver: sqlite3.Database
+//   })
+// })()
+
 async function openDB(filename) {
   return open({
     filename,
@@ -12,34 +22,46 @@ async function openDB(filename) {
   })
 }
 
-
-async function SELECT(sql, callback) {
-  let results
-  db.serialize(() => {
-    db.each(sql, async (err, row) => {
-      results = await row
-      callback(results)
-    })
-  })
-  db.close()
+async function SELECT(sql) {
+  const db = await openDB('./sunshine.db')
+  const result = await db.all(sql)
+  return result
 }
 
-function showData(data) {
-  let cols = Object.keys(data)
-  // console.log(`${cols[0]}: ${data[cols[0]]}, ${cols[1]}: ${data[cols[1]]}`)
-  console.log(data)
-  return data
-}
+
+
+// async function SELECT(sql, callback) {
+//   let results
+//   db.serialize(() => {
+//     db.each(sql, async (err, row) => {
+//       results = await row
+//       callback(results)
+//     })
+//   })
+//   db.close()
+// }
+
+// function showData(data) {
+//   let cols = Object.keys(data)
+//   // console.log(`${cols[0]}: ${data[cols[0]]}, ${cols[1]}: ${data[cols[1]]}`)
+//   console.log(data)
+//   return data
+// }
+
+// async function test() {
+//   let output = await SELECT("Select * from test")
+//   console.log(output)
+// }
 
 async function test() {
   let output = await SELECT("Select * from test")
   console.log(output)
 }
 
-// test()
+
+test()
 
 module.exports = {
-  openDB,
   SELECT
 }
 
